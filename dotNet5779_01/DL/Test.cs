7d385d894b7bdd7b868f642d6cfa307f9c5ac34b
shlomi perry <SlomiPerry@gmail.com>
2018-12-22 23:52:39 +0200

    gfd

--- dotNet5779_01/DL/Test.cs
+++ dotNet5779_01/DL/Test.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    class Test
+    public class Test
     {
     }
 }
--- dotNet5779_01/DL/Tester.cs
+++ dotNet5779_01/DL/Tester.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    class Tester
+    public class Tester
     {
     }
 }
