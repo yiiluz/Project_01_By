97adca48ebf23bbfe2ebfe366f7825ca76c805aa
Yitzhak Iluz <Yitzhak.Iluz@gmail.com>
2018-12-22 22:58:29 +0200

    first commit

---
+++ dotNet5779_01/DL/Address.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public struct Address
+    {
+        private string city;
+        private string street;
+        private int buildingNumber;
+
+        /// <summary>
+        /// Argument ctor of struct Address
+        /// </summary>
+        /// <param name="city"></param>
+        /// <param name="street"></param>
+        /// <param name="building number"></param>
+        public Address(string c, string s, int n)
+        {
+            city = c;
+            street = s;
+            buildingNumber = n;
+
+        }
+        public string City
+        {
+            get
+            {
+                return city;
+            }
+            set
+            {
+                city = value;
+            }
+        }
+        public string Street
+        {
+            get
+            {
+                return street;
+            }
+            set
+            {
+                street = value;
+            }
+        }
+        public int BuildingNumber
+        {
+            get
+            {
+                return buildingNumber;
+            }
+            set
+            {
+                buildingNumber = value;
+            }
+        }
+        public override string ToString()
+        {
+            string tmp = "City: " + City + ".\nStreet: " + Street + ". Building number: " + BuildingNumber + ".\n";
+            return tmp;
+        }
+    }
+}
---
+++ dotNet5779_01/DL/Trainee.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DO
+{
+    public class Trainee
+    {
+        readonly string id;
+        private string lastName;
+        private string firstName;
+        private string schoolName;
+        private string teacherName;
+        private string phoneNumber;
+        private GenderEnum gender;
+        private Address address = new Address();
+        private DateTime dateOfBirth = new DateTime();
+        private DateTime lastTest = new DateTime();
+        private CarTypeEnum currCarType;
+        private GearboxTypeEnum currGearType;
+        private int numOfFinishedLessons;
+        private int numOfTests;
+        private bool isAlreadyDidTest;
+        private List<CarTypeEnum> existingLicenses = new List<CarTypeEnum>();
+
+        public string LastName { get => lastName; set => lastName = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string SchoolName { get => schoolName; set => schoolName = value; }
+        public string TeacherName { get => teacherName; set => teacherName = value; }
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+        public GenderEnum Gender { get => gender; set => gender = value; }
+        public Address Address { get => address; set => address = value; }
+        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public DateTime LastTest { get => lastTest; set => lastTest = value; }
+        public CarTypeEnum CurrCarType { get => currCarType; set => currCarType = value; }
+        public GearboxTypeEnum CurrGearType { get => currGearType; set => currGearType = value; }
+        public int NumOfFinishedLessons { get => numOfFinishedLessons; set => numOfFinishedLessons = value; }
+        public int NumOfTests { get => numOfTests; set => numOfTests = value; }
+        public bool IsAlreadyDidTest { get => isAlreadyDidTest; set => isAlreadyDidTest = value; }
+        public List<CarTypeEnum> ExistingLicenses { get => existingLicenses; set => existingLicenses = value; }
+        public string Id { get => id; }
+
+        public override string ToString()
+        {
+            string existingLic = "";
+            foreach (var item in ExistingLicenses)
+                existingLic += item + ", ";
+            existingLic.Remove(existingLic.Length - 2, existingLic.Length);
+            string tmp = "Trainee name: " + FirstName + " " + LastName + ".\nID: " + Id + ".\nGender: " + Gender + ".\nDate Of Birth: " + DateOfBirth.ToShortDateString() +
+                ".\nPhone number: " + PhoneNumber + ".\nAddress: " + Address + ".\nExisting linsences: " + existingLic +
+                ".\nType of current Gearbox: " + CurrGearType + ".\nType of current Car: " + CurrCarType + ".\nSchool name: " + SchoolName +
+                ".\nTeacher name: " + TeacherName + ".\nSum of pased lessons: " + numOfFinishedLessons + ".\n";
+            return tmp;
+        }
+    }
+}
