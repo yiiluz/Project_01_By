0b15e5b3a18d6c4e57912815bb98a5457ac66af7
Yitzhak Iluz <Yitzhak.Iluz@gmail.com>
2018-12-23 15:38:55 +0200

    update

--- dotNet5779_01/DL/Trainee.cs
+++ dotNet5779_01/DL/Trainee.cs
@@ -6,17 +6,9 @@ using System.Threading.Tasks;
 
 namespace DO
 {
-    public class Trainee
+    public class Trainee : Person
     {
-        readonly string id;
-        private string lastName;
-        private string firstName;
-        private string schoolName;
-        private string teacherName;
-        private string phoneNumber;
-        private GenderEnum gender;
-        private Address address = new Address();
-        private DateTime dateOfBirth = new DateTime();
+        
         private DateTime lastTest = new DateTime();
         private CarTypeEnum currCarType;
         private GearboxTypeEnum currGearType;
@@ -29,18 +21,27 @@ namespace DO
         /// default ctor
         /// </summary>
         /// <param name="id"></param>
-        public Trainee(string id)
+        public Trainee(string id) : base(id) { }
+
+        public Trainee(Trainee other) : base (other.Id)
         {
-            this.id = id;
+            LastName = other.LastName;
+            FirstName = other.FirstName;
+            SchoolName = other.SchoolName;
+            TeacherName = other.TeacherName;
+            PhoneNumber = other.PhoneNumber;
+            Gender = other.Gender;
+            Address = new Address(other.Address);
+            DateOfBirth = other.DateOfBirth;
+            LastTest = new DateTime(other.LastTest.Ticks);
+            CurrCarType = other.CurrCarType;
+            CurrGearType = other.CurrGearType;
+            NumOfFinishedLessons = other.NumOfFinishedLessons;
+            NumOfTests = other.NumOfTests;
+            IsAlreadyDidTest = other.IsAlreadyDidTest;
+            ExistingLicenses = new List<CarTypeEnum>(other.ExistingLicenses);
         }
-        public string LastName { get => lastName; set => lastName = value; }
-        public string FirstName { get => firstName; set => firstName = value; }
-        public string SchoolName { get => schoolName; set => schoolName = value; }
-        public string TeacherName { get => teacherName; set => teacherName = value; }
-        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
-        public GenderEnum Gender { get => gender; set => gender = value; }
-        public Address Address { get => address; set => address = value; } 
-        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+
         public DateTime LastTest { get => lastTest; set => lastTest = value; }
         public CarTypeEnum CurrCarType { get => currCarType; set => currCarType = value; }
         public GearboxTypeEnum CurrGearType { get => currGearType; set => currGearType = value; }
@@ -48,7 +49,7 @@ namespace DO
         public int NumOfTests { get => numOfTests; set => numOfTests = value; }
         public bool IsAlreadyDidTest { get => isAlreadyDidTest; set => isAlreadyDidTest = value; }
         public List<CarTypeEnum> ExistingLicenses { get => existingLicenses; set => existingLicenses = value; }
-        public string Id { get => id; }
+
 
         /// <summary>
         /// override of ToString
